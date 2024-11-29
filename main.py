8a017134c807fb654a0d3759bf804ffabb65dc91
DivProC <divij712@gmail.com>
2024-11-29 11:38:25 +0530

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 def min_coins(coins, target_amount):
-    ""code here""
+    store = [target_amount+1 for i in range(target_amount+1)]
+    store[0] = 0
+
+    for amount in range(1,target_amount+1):
+        for coin in coins:
+            if amount - coin >= 0:
+                store[amount] = min(store[amount], 1+ store[amount -coin])
+    
+    return store[target_amount] if store[target_amount] != target_amount + 1 else -1
